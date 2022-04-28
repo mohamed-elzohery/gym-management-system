3b0e59d6d1c1b510cd686504fd61319dae0347ac
MohamedGado <boogado@yahoo.com>
2022-04-28 20:13:45 +0200

    Running seeders with db:seed only

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
+            PermissionSeeder::class,
             AdminUserSeeder::class,
+            UserSeeder::class,
+            CitySeeder::class,
         ]);
     }
 }
