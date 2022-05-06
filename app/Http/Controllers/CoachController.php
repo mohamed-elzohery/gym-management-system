3c29e8ab97dae9f394a51ef7b68f99f8deee6a30
saraabdalla238 <saraabdallahamd@gmail.com>
2022-05-06 16:41:43 +0200

    edit into update coach function

--- app/Http/Controllers/CoachController.php
+++ app/Http/Controllers/CoachController.php
@@ -1,7 +1,9 @@
 
 <!-- //coach controller / -->
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Models\City;
 use Illuminate\Http\Request;
 use App\Models\Gym;
@@ -10,7 +12,7 @@ use Illuminate\Support\Facades\File;
 
 class CoachController extends Controller
 {
-// list function 
+    //list fun :
     public function list()
     {
         $coachesFromDB = User::role('coach')->withoutBanned()->get();
@@ -18,25 +20,26 @@ class CoachController extends Controller
             return view('empty');
         }
         return view("coach.list", ['coaches' => $coachesFromDB]);
-    }
-//show fun :-
+    } 
+    //show fun 
     public function show($id)
     {
         $singleCoach = User::find($id);
         return view("coach.show", ['singleCoach' => $singleCoach]);
     }
 //create fun :-
-public function create()
-{
-    $coaches = User::all();
-    $cities = City::all();
-    return view('coach.create', [
-        'users' => $coaches,
-        'cities' => $cities,
-    ]);
-}
-//update fun :-
-public function update(Request $request, $id)
+    public function create()
+    {
+        $coaches = User::all();
+        $cities = City::all();
+        return view('coach.create', [
+            'users' => $coaches,
+            'cities' => $cities,
+        ]);
+    }
+    
+//update fun
+    public function update(Request $request, $id)
     {
 
         $user = User::find($id);
@@ -45,4 +48,22 @@ public function update(Request $request, $id)
             'email' => 'required|string|unique:users,email,' . $user->id,
             'profile_image' => 'mimes:jpg,jpeg',
         ]);
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        if ($request->hasFile('profile_image')) {
+            $image = $request->file('profile_image');
+            $name = time() . \Str::random(30) . '.' . $image->getClientOriginalExtension();
+            $destinationPath = public_path('/imgs');
+            $image->move($destinationPath, $name);
+            $imageName = 'imgs/' . $name;
+            if (isset($user->profile_image))
+                File::delete(public_path('imgs/' . $user->profile_image));
+            $user->profile_image = $imageName;
+        }
+        $user->save();
+        return redirect()->route('coach.list');
+    }
+
 }
